601f4efaa2c4a9a25341d5fe069fd6a1641f918d
priyanka <priyankakumari6709@gmail.com>
2026-06-05 19:16:23 +0530

    Delete API

--- src/app.js
+++ src/app.js
@@ -5,7 +5,7 @@ const User = require("./models/user");
 
 app.use(express.json());
 app.post("/signup", async (req, res) => {
- // console.log(req.body);
+  // console.log(req.body);
   //creating instance of userModdel
   const user = new User(req.body);
   try {
@@ -16,6 +16,38 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.get("/user", async (req, res) => {
+  const userEmail = req.body.emailId;
+  try {
+    const user = await User.find({ emailId: userEmail });
+    res.send(user);
+  } catch (err) {
+    res.status(400).send("User not found");
+  }
+});
+
+
+app.get("/feed", async(req, res) => {
+  try { 
+    const users = await User.find({})
+    res.send(users);
+  } catch (err) {
+    res.status(400).send("User feed not found");
+  }
+});
+
+app.delete("/user",async(req,res)=>{
+    const userId =  req.body._id;
+    try
+    {
+        const user = await User.findByIdAndDelete(userId);
+        res.send("User deleted successfully")
+    }
+    catch(err){
+        res.status(400).send("User not deleted")
+    }
+})
+
 connectDB()
   .then(() => {
     console.log("Database connection establish");
@@ -24,5 +56,5 @@ connectDB()
     });
   })
   .catch((err) => {
-    console.error("Database cannot be connected"+err.message);
+    console.error("Database cannot be connected" + err.message);
   });
